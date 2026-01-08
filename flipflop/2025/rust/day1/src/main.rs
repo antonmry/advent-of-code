faee60276fd5c624cec11e05767d989a533f7898
Anton Rodriguez <anton@phoebe.ai>
2026-01-08 23:27:07 +0100

    FlipFlop: 2025: day 1. Third pointer

--- flipflop/2025/rust/day1/src/main.rs
+++ flipflop/2025/rust/day1/src/main.rs
@@ -7,17 +7,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("./input")?;
     let reader = BufReader::new(file);
 
-    let halves: Vec<usize> = reader
-        .lines()
-        .map_while(|l| l.ok())
-        .map(|l| l.len() / 2)
-        .collect();
+    let halves: Vec<String> = reader.lines().map_while(|l| l.ok()).collect();
 
-    let result: usize = halves.iter().sum();
+    let result: usize = halves.iter().map(|l| l.len() / 2).sum();
     assert_eq!(result, 10233);
 
-    let result: usize = halves.iter().filter(|b| b.rem_euclid(2) == 0).sum();
+    let result: usize = halves
+        .iter()
+        .map(|l| l.len() / 2)
+        .filter(|b| b.rem_euclid(2) == 0)
+        .sum();
     assert_eq!(result, 5226);
+
+    let result: usize = halves
+        .iter()
+        .filter(|b| !b.contains("ne"))
+        .map(|l| l.len() / 2)
+        .sum();
+    assert_eq!(result, 3060);
 
     Ok(())
 }
@@ -29,7 +36,7 @@ mod tests {
     // use super::*;
 
     #[test]
-    fn day1() {
+    fn default() {
         let input = "banana
 banenanana
 bananana
@@ -38,32 +45,23 @@ bananananana";
 
         let reader = Cursor::new(input);
 
-        let result: usize = reader
-            .lines()
-            .map_while(|l| l.ok())
-            .map(|l| l.len() / 2)
-            .sum();
+        let halves: Vec<String> = reader.lines().map_while(|l| l.ok()).collect();
 
+        let result: usize = halves.iter().map(|l| l.len() / 2).sum();
         assert_eq!(result, 24);
-    }
 
-    #[test]
-    fn day2() {
-        let input = "banana
-banenanana
-bananana
-bananananana
-bananananana";
-
-        let reader = Cursor::new(input);
-
-        let result: usize = reader
-            .lines()
-            .map_while(|l| l.ok())
+        let result: usize = halves
+            .iter()
             .map(|l| l.len() / 2)
             .filter(|b| b.rem_euclid(2) == 0)
             .sum();
-
         assert_eq!(result, 16);
+
+        let result: usize = halves
+            .iter()
+            .filter(|b| !b.contains("ne"))
+            .map(|l| l.len() / 2)
+            .sum();
+        assert_eq!(result, 19);
     }
 }
