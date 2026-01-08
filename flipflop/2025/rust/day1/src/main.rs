3782b2907cbb5dbae4192e2ffa7b71e254aae347
Anton Rodriguez <anton@phoebe.ai>
2026-01-08 23:18:26 +0100

    FlipFlop: 2025: day 1. Second pointer

--- flipflop/2025/rust/day1/src/main.rs
+++ flipflop/2025/rust/day1/src/main.rs
@@ -1,16 +1,23 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("./input")?;
     let reader = BufReader::new(file);
 
-    let result: usize = reader
+    let halves: Vec<usize> = reader
         .lines()
         .map_while(|l| l.ok())
         .map(|l| l.len() / 2)
-        .sum();
+        .collect();
 
-    assert_eq!(result, 24);
+    let result: usize = halves.iter().sum();
+    assert_eq!(result, 10233);
+
+    let result: usize = halves.iter().filter(|b| b.rem_euclid(2) == 0).sum();
+    assert_eq!(result, 5226);
 
     Ok(())
 }
@@ -38,5 +45,25 @@ bananananana";
             .sum();
 
         assert_eq!(result, 24);
+    }
+
+    #[test]
+    fn day2() {
+        let input = "banana
+banenanana
+bananana
+bananananana
+bananananana";
+
+        let reader = Cursor::new(input);
+
+        let result: usize = reader
+            .lines()
+            .map_while(|l| l.ok())
+            .map(|l| l.len() / 2)
+            .filter(|b| b.rem_euclid(2) == 0)
+            .sum();
+
+        assert_eq!(result, 16);
     }
 }
