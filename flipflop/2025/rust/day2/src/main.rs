62a0a08a58d688fc0293249f85b60f5e25e134e8
Anton Rodriguez <anton@phoebe.ai>
2026-01-10 00:38:08 +0100

    FlipFlop: 2025: day 2. Third pointer

--- flipflop/2025/rust/day2/src/main.rs
+++ flipflop/2025/rust/day2/src/main.rs
@@ -12,10 +12,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(152, result);
     let result = find_max_height2(&line);
     assert_eq!(1583, result);
+    let result = find_max_height3(&line);
+    assert_eq!(42797, result);
     Ok(())
 }
 
-fn find_max_height(input: &str) -> i32 {
+fn find_max_height(input: &str) -> isize {
     input
         .chars()
         .fold((0, 0), |(height, max), c| {
@@ -30,31 +32,52 @@ fn find_max_height(input: &str) -> i32 {
         .1
 }
 
-fn find_max_height2(input: &str) -> i32 {
+fn find_max_height2(input: &str) -> isize {
     input
         .chars()
-        .fold((0, 0, 0), |(height, max, buffer), c| {
-            let new_buffer = match c {
+        .fold((0, 0, 0), |(height, max, streak), c| {
+            let streak = match c {
+                '^' if streak >= 0 => streak + 1,
+                '^' => 1,
+                'v' if streak <= 0 => streak - 1,
+                'v' => -1,
+                _ => 0,
+            };
+            let new_height = height + streak;
+            (new_height, new_height.max(max), streak)
+        })
+        .1
+}
+
+fn find_max_height3(input: &str) -> isize {
+    input
+        .chars()
+        .fold((0, 0, 0), |(height, max, streak), c| {
+            let mut new_height = height;
+            let new_streak = match c {
+                '^' if streak > 0 => streak + 1,
                 '^' => {
-                    if buffer >= 0 {
-                        buffer + 1
-                    } else {
-                        1
-                    }
+                    new_height = height - fib(streak);
+                    1
                 }
+                'v' if streak < 0 => streak - 1,
                 'v' => {
-                    if buffer <= 0 {
-                        buffer - 1
-                    } else {
-                        -1
-                    }
+                    new_height = height + fib(streak);
+                    -1
                 }
                 _ => 0,
             };
-            let new_height = height + new_buffer;
-            (new_height, new_height.max(max), new_buffer)
+            (new_height, max.max(new_height), new_streak)
         })
         .1
+}
+
+fn fib(input: isize) -> isize {
+    match input.abs() {
+        0 => 0,
+        1 => 1,
+        n => fib(n - 1) + fib(n - 2),
+    }
 }
 
 #[cfg(test)]
@@ -69,5 +92,14 @@ mod tests {
     #[test]
     fn test_max_height2() {
         assert_eq!(15, find_max_height2("^^^v^^^^vvvvvvv"));
+    }
+
+    #[test]
+    fn test_max_height3() {
+        assert_eq!(144, fib(12));
+        assert_eq!(5, fib(5));
+        // assert_eq!(5, count_streak("^^^^^")); // This case doesn't work!
+        assert_eq!(4, find_max_height3("^^^v^^^^vvvvvvv"));
+        assert_eq!(144, find_max_height3("^^^^^^^^^^^^vvvvvvvvv^"));
     }
 }
