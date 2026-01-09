63ddbab3fbcd8e8fad1c28725ba12a35dd88cf1a
Anton Rodriguez <anton@phoebe.ai>
2026-01-09 19:17:06 +0100

    Flipflop: 2025: day 1. Second pointer

--- flipflop/2025/rust/day2/src/main.rs
+++ flipflop/2025/rust/day2/src/main.rs
@@ -9,8 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let line = reader.lines().next().unwrap()?;
 
     let result = find_max_height(&line);
-
     assert_eq!(152, result);
+    let result = find_max_height2(&line);
+    assert_eq!(1583, result);
     Ok(())
 }
 
@@ -29,6 +30,33 @@ fn find_max_height(input: &str) -> i32 {
         .1
 }
 
+fn find_max_height2(input: &str) -> i32 {
+    input
+        .chars()
+        .fold((0, 0, 0), |(height, max, buffer), c| {
+            let new_buffer = match c {
+                '^' => {
+                    if buffer >= 0 {
+                        buffer + 1
+                    } else {
+                        1
+                    }
+                }
+                'v' => {
+                    if buffer <= 0 {
+                        buffer - 1
+                    } else {
+                        -1
+                    }
+                }
+                _ => 0,
+            };
+            let new_height = height + new_buffer;
+            (new_height, new_height.max(max), new_buffer)
+        })
+        .1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,5 +64,10 @@ mod tests {
     #[test]
     fn test_max_height() {
         assert_eq!(6, find_max_height("^^^v^^^^vvvvvvv"));
+    }
+
+    #[test]
+    fn test_max_height2() {
+        assert_eq!(15, find_max_height2("^^^v^^^^vvvvvvv"));
     }
 }
