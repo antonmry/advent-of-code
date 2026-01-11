557d7ebf31e80eaba3d0ca171dbc96bba58b4c2e
Anton Rodriguez <anton@phoebe.ai>
2026-01-12 00:05:21 +0100

    FlipFlop: 2025: day 3. Second pointer

--- flipflop/2025/rust/day3/src/main.rs
+++ flipflop/2025/rust/day3/src/main.rs
@@ -4,6 +4,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string("./input")?;
     let result = part1(&input);
     assert_eq!("71,50,92", result);
+    let result = part2(&input);
+    assert_eq!(687, result);
     Ok(())
 }
 
@@ -18,6 +20,21 @@ fn part1(input: &str) -> &str {
         .max_by_key(|(_, count)| *count)
         .map(|(line, _)| line)
         .unwrap_or("")
+}
+
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let mut c = line.split(',');
+
+            let red = c.next().unwrap().parse::<usize>().unwrap();
+            let green = c.next().unwrap().parse::<usize>().unwrap();
+            let blue = c.next().unwrap().parse::<usize>().unwrap();
+
+            (green > red) && (green > blue) && (blue != red)
+        })
+        .count()
 }
 
 #[cfg(test)]
@@ -35,5 +52,8 @@ mod tests {
 
         let result = part1(input);
         assert_eq!("10,20,30", result);
+
+        let result = part2(input);
+        assert_eq!(0, result);
     }
 }
