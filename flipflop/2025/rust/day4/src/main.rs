19f57d75d0a4d82689405fcef555684fedad0cef
Anton Rodriguez <anton@phoebe.ai>
2026-01-12 23:54:19 +0100

    Flipflop: 2025: day 4. Third pointer

--- flipflop/2025/rust/day4/src/main.rs
+++ flipflop/2025/rust/day4/src/main.rs
@@ -1,62 +1,55 @@
 use std::fs;
 
-struct Steps {
-    position: (usize, usize),
-    counter: usize,
-}
-
 fn main() {
     let input = fs::read_to_string("./input").unwrap();
     let result = part1(&input);
     assert_eq!(7069, result);
     let result = part2(&input);
     assert_eq!(4853, result);
+    let result = part3(&input);
+    assert_eq!(2067, result);
 }
 
 fn part1(input: &str) -> usize {
-    input
-        .lines()
-        .map(|m| {
-            let p = m.split_once(',').unwrap();
-            (p.0.parse::<usize>().unwrap(), p.1.parse::<usize>().unwrap())
+    parse_points(input)
+        .fold(((0, 0), 0), |(pos, counter), x| {
+            (x, counter + pos.0.abs_diff(x.0) + pos.1.abs_diff(x.1))
         })
-        .fold(
-            Steps {
-                position: (0, 0),
-                counter: 0,
-            },
-            |acc, x| Steps {
-                position: (x.0, x.1),
-                counter: acc.counter + acc.position.0.abs_diff(x.0) + acc.position.1.abs_diff(x.1),
-            },
-        )
-        .counter
+        .1
 }
 
 fn part2(input: &str) -> usize {
-    input
-        .lines()
-        .map(|m| {
-            let p = m.split_once(',').unwrap();
-            (p.0.parse::<usize>().unwrap(), p.1.parse::<usize>().unwrap())
+    parse_points(input)
+        .fold(((0, 0), 0), |(pos, counter), x| {
+            (x, counter + part2_distance(x, pos))
         })
-        .fold(
-            Steps {
-                position: (0, 0),
-                counter: 0,
-            },
-            |acc, x| Steps {
-                position: (x.0, x.1),
-                counter: acc.counter + part2_distance(x, acc.position),
-            },
-        )
-        .counter
+        .1
 }
 
 fn part2_distance(o: (usize, usize), i: (usize, usize)) -> usize {
     let x = o.0.abs_diff(i.0);
     let y = o.1.abs_diff(i.1);
-    x.min(y) + x.abs_diff(y)
+    x.max(y)
+}
+
+fn part3(input: &str) -> usize {
+    let mut trash: Vec<_> = parse_points(input).collect();
+
+    trash.sort_by_key(|t| t.0 + t.1);
+
+    trash
+        .iter()
+        .fold(((0, 0), 0), |(pos, counter), x| {
+            (*x, counter + part2_distance(*x, pos))
+        })
+        .1
+}
+
+fn parse_points(input: &str) -> impl Iterator<Item = (usize, usize)> + '_ {
+    input.lines().map(|m| {
+        let p = m.split_once(',').unwrap();
+        (p.0.parse::<usize>().unwrap(), p.1.parse::<usize>().unwrap())
+    })
 }
 
 #[cfg(test)]
@@ -79,5 +72,15 @@ mod tests {
         assert_eq! {part2_distance((9,9), (6,6)), 3};
         assert_eq! {part2_distance((3,2), (6,6)), 4};
         assert_eq! {part2_distance((0,0), (0,6)), 6};
+    }
+
+    #[test]
+    fn part3test() {
+        let input = "3,3
+9,9
+6,6";
+
+        let result = part3(input);
+        assert_eq!(9, result);
     }
 }
