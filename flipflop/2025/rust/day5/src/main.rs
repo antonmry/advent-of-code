84fcddac21ec705d88b7e41153d05e8f76a75d2a
Anton Rodriguez <anton@phoebe.ai>
2026-01-13 23:35:28 +0100

    FlipFlop: 2025: day 5. Second pointer

--- flipflop/2025/rust/day5/src/main.rs
+++ flipflop/2025/rust/day5/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./input").unwrap();
     assert_eq!(1553, part1(&input));
+    assert_eq!("GWaISOhXzdCTvPrm", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -27,6 +28,33 @@ fn part1(input: &str) -> usize {
     }
 }
 
+fn part2(input: &str) -> String {
+    let mut i = 0;
+    let mut visited = Vec::new();
+    loop {
+        let ch = input.chars().nth(i).unwrap();
+        visited.push(ch);
+        let begin = input.find(ch).unwrap();
+        let end = input.rfind(ch).unwrap();
+
+        if i == begin {
+            i = end + 1;
+        } else {
+            i = begin + 1;
+        }
+
+        if i - 1 == input.len() - 1 {
+            let mut result = Vec::new();
+            input.chars().for_each(|c| {
+                if !visited.contains(&c) && !result.contains(&c) {
+                    result.push(c);
+                }
+            });
+            return result.into_iter().collect();
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +63,11 @@ mod tests {
     fn test_part1() {
         let input = "ABccksiPiBAksP";
         assert_eq!(38, part1(input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "ABccksiPiBAksP";
+        assert_eq!("Bc", part2(input));
     }
 }
